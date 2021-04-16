032c60bfb01f3c5cc19d6498226c79aa8bbd8d87
Harpal <jadejaharpal14@gmail.com>
2021-04-16 21:42:33 +0530

    Migration to Spring Boot Completed

---
+++ emipaylelo/src/test/java/com/emipaylelo/EmipayleloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emipaylelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmipayleloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
